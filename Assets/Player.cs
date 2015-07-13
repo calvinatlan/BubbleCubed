4b96c23a4dc00b95e4a00343cdcf0768710dd9d8
calvinatlan <calvinatlan@gmail.com>
2015-07-12 21:16:15 -0700

    Created the prototype scene
    Movement is constricted to 3 lanes, and you cannot switch lanes if you are in the middle of switching.
    Light and Camera set up, but likely to be placeholders for a better camera angle and lighting.

---
+++ Assets/Player.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	//How far the cube moves
+	public float mD;
+	//How long the cube takes to switch lanes
+	public float speed;
+
+	private enum lanes {Left, Middle, Right};
+	private int curLane = (int)lanes.Middle;
+
+	//Variables used for positional interpolation
+	private Vector3 pos;
+	private Vector3 target;
+	private float startTime;
+	private bool moving = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movement ();
+	}
+
+	//This code checks if the player wants to move left or right, and makes sure they are not already moving
+	//Then it calls a coroutine which moves the player smoothly one lane over
+	void Movement(){
+		if (!moving) {
+			if (Input.GetAxis ("Horizontal") > 0 && curLane != (int)lanes.Right) {
+				pos = transform.position;
+				target = new Vector3 (pos.x + mD, pos.y, pos.z);
+				startTime = Time.time;
+				StartCoroutine ("Move");
+				curLane++;
+			} else if (Input.GetAxis ("Horizontal") < 0 && curLane != (int)lanes.Left) {
+				pos = transform.position;
+				target = new Vector3 (pos.x - mD, pos.y, pos.z);
+				startTime = Time.time;
+				StartCoroutine ("Move");
+				curLane--;
+			}
+		}
+	}
+
+	IEnumerator Move(){
+		moving = true;
+		float track = 0f;
+		while (track < 1f) {
+			track = (Time.time - startTime) / speed;
+			transform.position = Vector3.Lerp (pos, target, track);
+			yield return null;
+		}
+		moving = false;
+	}
+}
