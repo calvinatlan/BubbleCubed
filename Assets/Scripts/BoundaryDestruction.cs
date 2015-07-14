3848e4b319e251adc64d304499a1028a3049f676
calvinatlan <calvinatlan@gmail.com>
2015-07-13 23:12:08 -0700

    Added a trigger that destroys objects that leave the game screen

---
+++ Assets/Scripts/BoundaryDestruction.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoundaryDestruction : MonoBehaviour {
+
+	void Create(){
+
+	}
+
+	void Update() {
+
+	}
+
+	void OnTriggerEnter(Collider other){
+		print (other);
+		Destroy (other.gameObject);
+	}
+}
