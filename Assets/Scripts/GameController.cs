a096bb1a107008a0ea3d298f715ba2f707907373
NirStudnitski <nir.stud@gmail.com>
2015-07-14 10:47:23 -0700

    lane and camera angle fix

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,6 +5,7 @@ public class GameController : MonoBehaviour
 {
 	public GameObject lane;
 	public float laneWait;
+	public float laneGenPointZ;
 	
 	void Start ()
 	{
@@ -13,10 +14,16 @@ public class GameController : MonoBehaviour
 	
 	IEnumerator SpawnLanes ()
 	{
-		
+		//generate first line of lanes
+		for (int i = 0; i<laneGenPointZ; i++) 
+		{
+			Vector3 initialPosition = new Vector3 (0, -1, i);
+			Quaternion spawnRotation = Quaternion.identity * Quaternion.Euler(90f,0f,0f);
+			Instantiate (lane, initialPosition, spawnRotation);
+		}
 		while (true)
 		{
-			Vector3 spawnPosition = new Vector3 (0, -1, 10);
+			Vector3 spawnPosition = new Vector3 (0, -1, laneGenPointZ);
 			Quaternion spawnRotation = Quaternion.identity * Quaternion.Euler(90f,0f,0f);
 			Instantiate (lane, spawnPosition, spawnRotation);
 			yield return new WaitForSeconds (laneWait);
