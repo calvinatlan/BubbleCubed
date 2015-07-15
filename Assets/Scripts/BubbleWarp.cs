6c24b8fd52fcfc1cba6581ac608047669de4f22f
pokeimon <imondaneshmand@gmail.com>
2015-07-15 00:08:56 -0700

    Added to the bubble warp (horribly buggy and not working)

--- Assets/Scripts/BubbleWarp.cs
+++ Assets/Scripts/BubbleWarp.cs
@@ -2,9 +2,12 @@
 using System.Collections;
 
 public class BubbleWarp : MonoBehaviour {
+	public float slope = 0.5f;
+	public float interval = .01f;
 	private Vector3 start;
-	public float warpScale;
-	//private int x, y, z;
+	private float smooth = 0.5f;
+	private float tNext = 0f;
+	private float dir = 1f;
 
 
 
@@ -12,11 +15,22 @@ public class BubbleWarp : MonoBehaviour {
 
 
 	void Start () {
-		start = transform.localScale;
+		//start = transform.localScale;
 	}
 
 	// Update is called once per frame
 	void Update () {
+		if (Time.time > tNext) {
+			tNext += interval*(0.5f+Random.value);
+			dir = -dir;
+		}
+		smooth += dir * slope * Time.deltaTime;
+		if (smooth > 1f || smooth < 0.5f) {
+			dir = -dir;
+		}
+		smooth = Mathf.Clamp (smooth, 0f, 0.5f);
+		transform.localScale += new Vector3 (smooth*dir, 0f, 0f);
+
 						
 	}
 }
