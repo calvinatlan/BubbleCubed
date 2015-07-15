78e53194767c02efa71b388644c9085a0c6fc699
calvinatlan <calvinatlan@gmail.com>
2015-07-14 22:55:46 -0700

    Got a mobile version running!

--- Assets/TouchController.cs
+++ Assets/TouchController.cs
@@ -3,12 +3,19 @@ using System.Collections;
 
 public class TouchController : MonoBehaviour {
 
+	//Main cube
+	private Player p;
+
+	//For the swipes
 	private Vector2 tDel;
 	private float absMag;
 
+	//For the tap
+	private bool isTap;
+
 	// Use this for initialization
 	void Start () {
-	
+		p = (Player) this.gameObject.GetComponent("Player");
 	}
 	
 	// Update is called once per frame
@@ -16,6 +23,7 @@ public class TouchController : MonoBehaviour {
 
 		//Do nothing if no fingers are down
 		if (Input.touchCount == 0) {
+			isTap = true;
 			return;
 		}
 
@@ -24,7 +32,7 @@ public class TouchController : MonoBehaviour {
 		absMag = Mathf.Sqrt (tDel.magnitude);
 
 		if (absMag > 5) {
-			Player p = (Player) this.gameObject.GetComponent("Player");
+			isTap = false;
 			if(Mathf.Abs(tDel.x) > Mathf.Abs(tDel.y)){
 				if (tDel.x > 0){
 					p.rotateTo(2);
@@ -37,6 +45,13 @@ public class TouchController : MonoBehaviour {
 				}else{
 					p.rotateTo (3);
 				}
+			}
+		}
+		if (t.phase == TouchPhase.Ended && isTap) {
+			if(t.position.x < (Screen.width / 3)){
+				p.moveTo (0);
+			}else if (t.position.x > (2 * Screen.width / 3)){
+				p.moveTo (1);
 			}
 		}
 
