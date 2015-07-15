6b2716e4d7216e374a68f237ad21fd996f6c0e93
pokeimon <imondaneshmand@gmail.com>
2015-07-14 17:00:28 -0700

    Started the warp/wiggle for bubbles (very incomplete)

---
+++ Assets/Scripts/BubbleWarp.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubbleWarp : MonoBehaviour {
+	private Vector3 start;
+	public float warpScale;
+	//private int x, y, z;
+
+
+
+	// Use this for initialization
+
+
+	void Start () {
+		start = transform.localScale;
+	}
+
+	// Update is called once per frame
+	void Update () {
+						
+	}
+}
