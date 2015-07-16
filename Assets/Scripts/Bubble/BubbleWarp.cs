7d15ca5710dbdeca99481522d56e2e881d82fc70
Calvin Atlan <catlan@ucsc.edu>
2015-07-16 09:40:26 -0700

    Commented the warp code

--- Assets/Scripts/Bubble/BubbleWarp.cs
+++ Assets/Scripts/Bubble/BubbleWarp.cs
@@ -12,24 +12,34 @@ public class BubbleWarp : MonoBehaviour {
 
 
 	void Start () {
+		//Smallest bubble size
 		scaleS = transform.localScale;
+
+		//Largest bubble size
 		scaleL = new Vector3 (scaleS.x+.3f, scaleS.y+.3f, scaleS.z+.3f);
+
+		//The delta size of the bubble (growth/shrink per loop)
 		mini = new Vector3 (0.01f, 0.01f, 0.01f);
 //		rising = true;
 
 		StartCoroutine (changeSize ());
 	}
 
+
 	IEnumerator changeSize(){
 		while (true) {
+			//grow until you reach maximum size
 			while (transform.localScale.x < scaleL.x) {
 				transform.localScale += mini;
 				yield return new WaitForSeconds(1/60f);
 			}
+			//shrink until you reach minimum size
 			while (transform.localScale.x > scaleS.x) {
 				transform.localScale -= mini;
 				yield return new WaitForSeconds(1/60f);
 			}
+
+			//repeat forever until object is destroyed
 		}
 	}
 }
