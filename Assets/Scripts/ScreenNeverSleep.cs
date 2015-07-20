f0cd29b38a3351e5a9777c6bcdca8aecd6ccf5b6
ImonDaneshmand <imondaneshmand@gmail.com>
2015-07-19 19:43:56 -0700

    Prevents Screen Sleeping
    For andriod while the main game is running.

---
+++ Assets/Scripts/ScreenNeverSleep.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenNeverSleep : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.sleepTimeout = (int)SleepTimeout.NeverSleep;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
