1cfdd4088cd4d901cfe2e23e37aec06f79b1c412
NirStudnitski <nir.stud@gmail.com>
2015-07-22 13:46:25 -0700

    more heart detail

---
+++ Assets/HeartDestroy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeartDestroy : MonoBehaviour {
+	
+	//public GameObject explosionHeart;
+
+	
+	
+	
+	void OnTriggerEnter(Collider other) 
+	{
+
+			//AudioSource audio = GetComponent<AudioSource>();
+			//audio.Play();
+			//Destroy (other.gameObject);
+			this.transform.parent.GetComponent<Player>().counts++;
+			this.transform.parent.GetComponent<Player>().totalCount++;
+			this.transform.parent.GetComponent<Player>().health+=25;//here
+
+
+	}
+}
