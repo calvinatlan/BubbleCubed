49d53b7f24b9f0f9bbc980950c9e1462eee3f91d
NirStudnitski <nir.stud@gmail.com>
2015-07-22 15:06:12 -0700

    more heart code

---
+++ Assets/Scripts/Destruction/HeartDestroy.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeartDestroy : MonoBehaviour {
+	
+	//public GameObject explosionHeart;
+
+	private GameObject cube;
+	private Player player;
+
+	void Start ()
+	{
+		cube = GameObject.FindGameObjectWithTag ("Player");
+		player = cube.GetComponent<Player> ();
+
+	}
+	void OnTriggerEnter(Collider other) 
+	{
+
+		if (other.tag == "Player") 
+		{
+			player.points(25);
+			//AudioSource audio = GetComponent<AudioSource>();
+			//audio.Play();
+			//Destroy (other.gameObject);
+			//this.transform.parent.GetComponent<Player> ().counts++;
+			//this.transform.parent.GetComponent<Player> ().totalCount++;
+			//this.transform.parent.GetComponent<Player> ().health += 25;//here
+
+			Destroy(this.gameObject);
+		}
+
+
+	}
+}
