24a72ceb4422963e03e62b95f30493b7486c67fd
NirStudnitski <nir.stud@gmail.com>
2015-07-23 13:04:13 -0700

    small change

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -67,7 +67,7 @@ public class GameController : MonoBehaviour
 		{
 			//difficulty randomizer
 			System.Random rnd = new System.Random();
-			/*
+
 			int chooser = rnd.Next (1,1001);
 			int color;
 			if (chooser < 500- (int) difficulty * 300) color = rnd.Next (1,4);
@@ -77,8 +77,6 @@ public class GameController : MonoBehaviour
 			else if (chooser < 1000- (int) difficulty * 50)color = 7;
 			else color =4;
 
-*/
-			int color = 7;
 			spawner.createBubble (color);
 			yield return new WaitForSeconds (waveWait);
 		}
