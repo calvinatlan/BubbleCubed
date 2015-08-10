9bfc834014d429c16411ff3c77a65a2bf44b458b
NirStudnitski <nir.stud@gmail.com>
2015-08-10 16:01:06 -0700

    Rainbow heart is now pimp-ass

---
+++ Assets/Scripts/ParticleRotatorRainbow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleRotatorRainbow : MonoBehaviour {
+	
+	public float rotationRadius;
+	public float rotationSpeed;
+	public float delay;
+	public float yOffset;
+	
+	
+	void Update() 
+	{
+		Transform parentLoc = gameObject.transform.parent.transform;
+		float xParent = parentLoc.position.x;
+		float yParent = parentLoc.position.y;
+		float zParent = parentLoc.position.z;
+		float xPosition = (Mathf.Sin(Time.time*rotationSpeed+ delay) * Time.deltaTime*rotationRadius) + xParent;
+		float yPosition = (Mathf.Sin(Time.time*rotationSpeed+ delay) * Time.deltaTime*rotationRadius*yOffset) + yParent;
+		float zPosition = (Mathf.Cos(Time.time*rotationSpeed+ delay) * Time.deltaTime*rotationRadius) + zParent;
+		transform.position = new Vector3(xPosition, yPosition, zPosition);
+		
+	}
+}
