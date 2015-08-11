1c481e757d95de1fb3a9bd1f8b06dc162cb31129
ImonDaneshmand <imondaneshmand@gmail.com>
2015-08-11 10:30:29 -0700

    working on object pooling bubbles
    Currently using old method

---
+++ Assets/Scripts/Bubble/BubbleDestroy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubbleDestroy : MonoBehaviour {
+	
+	void OnEnable(){
+		Invoke("Destroy", 1f);
+	}
+	
+	void Destroy(){
+		gameObject.SetActive(false);
+	}
+	
+	void OnDisable(){
+		CancelInvoke();
+	}
+	
+}
